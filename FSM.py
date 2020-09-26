1ec12762b48a9afe2bc0a58d12eb7ae343406cd5
danielsdliu <70170755+danielsdliu@users.noreply.github.com>
2020-09-26 16:45:11 -0500

    Add files via upload

--- FSM.py
+++ FSM.py
@@ -21,7 +21,7 @@ def state2(step):
     # Our alphabet only allows 1 and 0s (and epsilon, but only for q1) so this will break the loop to return a
     # rejection.
     else:
-        return "invalid"
+        return "Invalid state"
 
 
 def state3(step):
@@ -30,7 +30,7 @@ def state3(step):
     elif step == "0":
         return "q3"
     else:
-        return "invalid"
+        return "Invalid state"
 
 
 def state4(step):
@@ -39,10 +39,12 @@ def state4(step):
     elif step == "0":
         return "q2"
     else:
-        return "invalid"
+        return "Invalid state"
 
 
-# Create function that evaluates what state you're in and makes the "move"
+# Create function that evaluates what state you're in and makes the "move."  This also means if you have a new
+# state, you can just make a new function like above and add it with a few lines.
+
 def stateeval(state, delta):
     if state == "q1":
         return state1(delta)
@@ -52,8 +54,8 @@ def stateeval(state, delta):
         return state3(delta)
     elif state == "q4":
         return state4(delta)
-    elif state == "invalid":
-        print("Invalid state")
+    else:
+        return "Invalid state"
 
 
 string = str(input("Enter your input string: \n"))
@@ -69,23 +71,21 @@ string = string.replace("\n", "")
 i = 0
 steps = ["q1"]
 
-# check your alphabet:
+# use while loop to make the steps. each step is appended to the list meaning you can map out the moves.
 while i < len(str(string)):
-    move = str(string)[i]
+    #    I initially used the commented code below but found I could consolidate it to one line.
     #    token = stateeval(token, step)
     #    steps.append(str(token))
-    steps.append(stateeval(steps[i], move))
+    steps.append(stateeval(steps[i], str(string)[i]))
+    # I had to play around with variable types but need str(string)[i] else it will read 1 or 0 as integers and
+    # be unable to compare.
     i += 1
 
 print("The moves are as follows:")
 print(steps)
 
+# Checks the final state you're in, if not in the accepting state (q4) then rejects.
 if steps[-1] == "q4":
     print("Your string was accepted.")
 else:
     print("Your string was rejected.")
-
-# if token == "q4":
-#    print("Your string was accepted.")
-# else:
-#    print("Your string was rejected.")
